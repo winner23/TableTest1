3c85987f40e3826f826c53ac4806b93ffb02318b
Volodymyr Viniarskyi <v.winner@gmail.com>
2018-03-26 14:57:22 +0300

    Initial Commit

---
+++ TableTest1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableTest1
+//
+//  Created by Volodymyr Viniarskyi on 3/26/18.
+//  Copyright © 2018 Volodymyr Viniarskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
