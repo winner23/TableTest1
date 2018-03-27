061fdf5e4e25f32d4266e983e1549e141de7b92e
Volodymyr Viniarskyi <v.winner@gmail.com>
2018-03-27 11:15:31 +0300

    need fix to add item

---
+++ TableTest1/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  TableTest1
+//
+//  Created by Volodymyr Viniarskyi on 3/26/18.
+//  Copyright © 2018 Volodymyr Viniarskyi. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TableTest1/ViewController.swift
+++ TableTest1/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var table: UITableView = UITableView()
+  var array = [(a:String, b:String)]()
+  
+  @IBAction func test(_ sender: UIBarButtonItem) {
+    guard let editViewController = UIStoryboard(name: "Main", bundle: nil)
+      .instantiateViewController(withIdentifier: "EditViewController") as? EditViewController else { return }
+    editViewController.navigationController?.title = "Add"
+    editViewController.result = {
+      //[unowned self]
+      (titleItem, descriptionItem) in
+      if titleItem.count>0 && descriptionItem.count>0 {
+        self.array.append((titleItem, descriptionItem))
+        self.table.reloadData()
+      }
+
+    }
+    
+    self.navigationController?.pushViewController(editViewController, animated: true)
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -20,6 +40,17 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell: UITableViewCell = UITableViewCell (style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+    cell.textLabel?.text = array[indexPath.row].a
+    cell.detailTextLabel?.text = array[indexPath.row].b
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return array.count
+  }
+}
